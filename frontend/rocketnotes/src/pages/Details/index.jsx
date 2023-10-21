c0099850b0695e91f2cd417d57c0457b2401704e
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-21 15:08:14 -0300

    Componente Header. Definindo estrutura grid rows para a plicação.

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,14 @@
 import { Container } from './styles';
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details () {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Felipe Sousa</span>
 
-      <Button title="Ver mais" loading/>
+      <Header />
+      <Button title="Voltar"/>
+      
     </Container>
   )
 }
