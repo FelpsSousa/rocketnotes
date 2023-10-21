dcca4c4f5c02da5cb722b16f26ad77fedd325a49
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-21 14:04:54 -0300

    Componente. Button.

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from './styles';
+import { Button } from '../../components/Button'
 
 export function Details () {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Felipe Sousa</span>
+
+      <Button />
     </Container>
   )
 }
