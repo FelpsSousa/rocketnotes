e6dd89e32dd7261b2333abdd0a021f4e6539bca9
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-21 15:22:36 -0300

    Imagem do usuário. Buscando a imagem diretamente do https://github.com.FelpsSousa.png. Componente Profile chamado dentro do componente Header.

--- frontend/rocketnotes/src/components/Header/index.jsx
+++ frontend/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container  } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
 
   return (
     <Container>
+      <Profile>
+        <img 
+          src="https://github.com/FelpsSousa.png"
+         alt="Foto do usuário" 
+        />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Felipe Sousa</strong>
+        </div>
+      </Profile>
 
     </Container>
   );
