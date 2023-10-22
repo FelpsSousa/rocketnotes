97233d073ba2a98ee0917d20ca13c5ddd6b36815
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-22 12:50:42 -0300

    Finalizando detalhes. Implementação dos detalhes da nota finalizado.

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { ButtonText } from '../../components/ButtonText';
@@ -8,27 +8,43 @@ import { Tag } from '../../components/Tag';
 export function Details () {
   return (
     <Container>
-
       <Header />
-      
-      <ButtonText title="Excluir nota" />
-      
-      <Section title="Links úteis">
-        <Links>
-          <li><a href='https://github.com/FelpsSousa'>GitHub</a></li>
-          <li><a href='https://linkedin.com/in/felipeluis-felpssousa'>LinkedIn</a></li>
-          <li>Link 3</li>
-        </Links>
-      </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/>
-      </Section>
-      
-      
-      <Button title="Voltar"/>
-      
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Qui numquam temporibus, animi dolorem aliquam exercitationem similique voluptas porro. 
+            Modi odit non ut soluta deleniti doloribus officia voluptatum molestiae animi blanditiis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Saepe officiis facere numquam soluta perferendis praesentium odio nihil illo possimus accusamus? 
+            Temporibus, velit ab error hic asperiores odit mollitia possimus! Dolores?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorem ratione corrupti incidunt, aliquam facilis doloremque quasi officia accusantium. 
+            Sapiente in dolorum pariatur tempore maxime rem vel nesciunt sequi illum.
+          </p>
+          
+          <Section title="Links úteis">
+            <Links>
+              <li><a href='https://github.com/FelpsSousa'>GitHub</a></li>
+              <li><a href='https://linkedin.com/in/felipeluis-felpssousa'>LinkedIn</a></li>
+              <li>Link 3</li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+          
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
