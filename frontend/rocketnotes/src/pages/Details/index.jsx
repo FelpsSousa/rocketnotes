f18bda6fe63188dc903f69283c07560c52c86641
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-22 08:38:48 -0300

    Children. Adicionado componente Section com propriedade children que é capza de setar todo o conteúdo dentro da Section.

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,12 +1,22 @@
-import { Container } from './styles';
-import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
+import { Container, Links } from './styles';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
 
 export function Details () {
   return (
     <Container>
 
       <Header />
+      
+      <Section title="Links úteis">
+        <Links>
+          <li><a href='https://github.com/FelpsSousa'>GitHub</a></li>
+          <li><a href='https://linkedin.com/in/felipeluis-felpssousa'>LinkedIn</a></li>
+          <li>Link 3</li>
+        </Links>
+      </Section>
+      
       <Button title="Voltar"/>
       
     </Container>
