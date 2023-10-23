b5e88962d4e0dba72c2c0d7c48b8076157217117
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-23 04:16:24 -0300

    Navegação. Navegação concluida em todas as págnas frontend. Tanto AppRoutes quanto AuthRoutes.

--- frontend/rocketnotes/src/pages/SignUp/index.jsx
+++ frontend/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -38,9 +39,9 @@ export function SignUp() {
         
         <Button title="Cadastrar" />
 
-        <a href="#">
+        <Link to="/">
           Voltar para o Login
-        </a>
+        </Link>
 
       </Form>
     </Container>
