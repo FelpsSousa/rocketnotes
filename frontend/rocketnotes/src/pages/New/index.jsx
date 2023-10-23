41a544c73489eba834ceb1abad751ac2203701c5
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-22 22:42:16 -0300

    Header da New.

--- frontend/rocketnotes/src/pages/New/index.jsx
+++ frontend/rocketnotes/src/pages/New/index.jsx
@@ -1,12 +1,23 @@
 import { Header } from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 export function New() {
 
   return (
     <Container>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+
+            <Input placeholder="Título" />
+          </header>
+        </Form>
+      </main>
     </Container>
   );
 }
